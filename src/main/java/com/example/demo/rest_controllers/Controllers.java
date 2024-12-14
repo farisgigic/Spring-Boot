cb6d240cbbe0e9f819b42a5fc95bc8c041e1f834
Faris <faris.gigic@stu.ibu.edu.ba>
2024-12-15 00:34:08 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/rest_controllers/Controllers.java
@@ -0,0 +1,15 @@
+package com.example.demo.rest_controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controllers {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "world") String name){
+        return "Hello, " + name;
+    }
+
+
+}
